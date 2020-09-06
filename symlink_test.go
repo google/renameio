d0469b59e6c3b324e9573f25c7bba39415677b88
Tom Payne <twpayne@gmail.com>
2020-09-06 23:14:09 +0200

    drop Windows support

--- symlink_test.go
+++ symlink_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// +build darwin dragonfly freebsd linux nacl netbsd openbsd solaris windows
+// +build !windows
 
 package renameio
 
