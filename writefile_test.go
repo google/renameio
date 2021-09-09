829673ecb1d2dfe5def22b7d0617c67fbc7e258a
Michael Hanselmann <public@hansmi.ch>
2021-09-09 19:50:20 +0200

    Add WriteFile equivalence tests
    Test that ioutil.WriteFile and renameio.WriteFile behave the same. This
    would've had a chance of catching the differing behaviour with the umask
    in the permissions.

--- writefile_test.go
+++ writefile_test.go
@@ -81,3 +81,101 @@ func TestWriteFileIgnoreUmask(t *testing.T) {
 		t.Errorf("got permissions %04o, want %04o", gotPerm, wantPerm)
 	}
 }
+
+func TestWriteFileEquivalence(t *testing.T) {
+	type writeFunc func(string, []byte, os.FileMode, ...Option) error
+	type test struct {
+		name   string
+		fn     writeFunc
+		perm   os.FileMode
+		umask  os.FileMode
+		exists bool
+	}
+
+	var tests []test
+
+	for _, wf := range []struct {
+		name string
+		fn   writeFunc
+	}{
+		{
+			name: "WriteFile",
+			fn:   WriteFile,
+		},
+		{
+			name: "ioutil",
+			fn: func(filename string, data []byte, perm os.FileMode, opts ...Option) error {
+				return ioutil.WriteFile(filename, data, perm)
+			},
+		},
+	} {
+		for _, perm := range []os.FileMode{0o755, 0o644, 0o400, 0o765} {
+			for _, umask := range []os.FileMode{0o000, 0o011, 0o007, 0o027, 0o077} {
+				for _, exists := range []bool{false, true} {
+					name := fmt.Sprintf("%s/perm%04o/umask%04o", wf.name, perm, umask)
+					if exists {
+						name += "/exists"
+					}
+
+					tests = append(tests, test{
+						name:   name,
+						fn:     wf.fn,
+						perm:   perm,
+						umask:  umask,
+						exists: exists,
+					})
+				}
+			}
+		}
+	}
+
+	const existingPerm os.FileMode = 0o654
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			withUmask(t, tc.umask)
+
+			maskedPerm := tc.perm & ^tc.umask
+
+			filename := filepath.Join(t.TempDir(), "test.txt")
+
+			if tc.exists {
+				// Create file in preparation for replacement
+				fh, err := os.Create(filename)
+				if err != nil {
+					t.Errorf("Create(%q) failed: %v", filename, err)
+				}
+
+				if err := fh.Chmod(existingPerm); err != nil {
+					t.Errorf("Chmod() failed: %v", err)
+				}
+
+				fh.Close()
+
+				maskedPerm = existingPerm
+			}
+
+			wantData := []byte("content\n")
+
+			if err := tc.fn(filename, wantData, tc.perm); err != nil {
+				t.Fatal(err)
+			}
+
+			gotData, err := ioutil.ReadFile(filename)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if !bytes.Equal(gotData, wantData) {
+				t.Errorf("got data %v, want data %v", gotData, wantData)
+			}
+
+			fi, err := os.Stat(filename)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if gotPerm := fi.Mode() & os.ModePerm; gotPerm != maskedPerm {
+				t.Errorf("got permissions %04o, want %04o", gotPerm, maskedPerm)
+			}
+		})
+	}
+}
